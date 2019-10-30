91e59de3df67f65b93e8ccbe05e173e334f1ccc9
kevin.clark@ctx.edu <kevin.uzziel.clark@gmail.com>
2019-10-30 18:35:00 -0500

    adding more

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,28 +1,63 @@
 // from data.js
 var tableData = data;
 
-function handleSubmit() {
-    d3.event.preventDefault();
-    const enterdate = d3.select("form-control").node()
-    console.log(enterdate);
+var tbody = d3.select("tbody");
 
-    // clear the input value
-        d3.select("form-control").node().value = "";
+
+
+function buildTable(table) {
+    table.forEach((sightings)=> {
+        var row=tbody.append("tr");
+        Object.entries(sightings).forEach(([key, value])=> {
+            var cell=tbody.append("td");
+            cell.text(value);
+        });
+
+    });
 }
 
 
-// data.forEach(function(datetime) {
-//     console.log(datetime);
-//      });
+var button = d3.select("#filter-btn");
+buildTable(tableData);
 
-// data.forEach(function(weatherReport) {
-// console.log(weatherReport);
-// let tbody = d3.select("tbody");
-// var row = tbody.append("tr");
+button.on("click", function(){
+    d3.event.preventDefault();
+    filterTable("#datatime")
+})
+  
 
-//     for (key in weatherReport){
-//        console.log(key, weatherReport[key]); 
-//         // Append a cell to the row for each value in the weather report object
-//        const cell = tbody.append("td");
-//      }
-// });
+
+function filterTable(datetime){
+  
+    
+    
+    var inputDate = d3.select(datetime).property("value");
+    var inputCity = d3.select("#city").property("value");
+    var inputState = d3.select("#state").property("value");
+    var inputCountry = d3.select("#country").property("value");
+    var inputShape = d3.select("#shape").property("value");
+
+    var checkTable =tableData.filter(tdata => tdata.datetime === inputValueDate || 
+        tdata.city === inputValueCity || tdata.state === inputValueState ||
+        tdata.country === inputValueCountry || tdata.shape === inputValueShape)
+
+    tbody.html("");
+    d3.select("span").html("");
+    
+    if(checkTable===undefined ||checkTable.length===0){
+        d3.select("span").text("No sightings on the data entered, please retry").style("font-size", "20px");
+    }
+    else{
+        buildTable(checkTable);
+    }
+
+    d3.select("#datetime").node().value = "";
+    d3.select("#city").node().value = "";
+    d3.select("#state").node().value = "";
+    d3.select("#country").node().value = "";
+    d3.select("#shape").node().value = "";
+
+};
+
+
+
