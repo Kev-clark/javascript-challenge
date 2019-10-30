6cb16fb7aa4a0b5c691e310a7a7892ad7c224f06
kevin.clark@ctx.edu <kevin.uzziel.clark@gmail.com>
2019-10-30 17:39:40 -0500

    push

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,4 +1,28 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+function handleSubmit() {
+    d3.event.preventDefault();
+    const enterdate = d3.select("form-control").node()
+    console.log(enterdate);
+
+    // clear the input value
+        d3.select("form-control").node().value = "";
+}
+
+
+// data.forEach(function(datetime) {
+//     console.log(datetime);
+//      });
+
+// data.forEach(function(weatherReport) {
+// console.log(weatherReport);
+// let tbody = d3.select("tbody");
+// var row = tbody.append("tr");
+
+//     for (key in weatherReport){
+//        console.log(key, weatherReport[key]); 
+//         // Append a cell to the row for each value in the weather report object
+//        const cell = tbody.append("td");
+//      }
+// });
